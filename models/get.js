521c39d780d43554d3b58396479eca1b25f1d081
prestaylor51 <prestaylor51@gmail.com>
2017-06-26 12:19:47 -0600

    stuff

--- models/get.js
+++ models/get.js
@@ -17,9 +17,35 @@ function getAllMentors(hobby,callback) {
     			.on('row', function(row) {
       				console.log(JSON.stringify(row));
     			});
-		});
-		
 
+
+    		console.log("finding mentors for %s", hobby);
+
+			var sql = "SELECT u.first, u.last, l.town, hm.mentor_id, h.name, hm.greeting FROM _user u \
+				JOIN hobby_mentor hm ON u.id = hm.mentor_id \
+				JOIN hobby h ON h.id = hm.hobby_id \
+				JOIN location l ON u.location_id = l.id \
+				WHERE h.name = $1 ;"
+
+			var params = [hobby];
+
+			var query = client.query(sql, params, function(err, result) {
+
+				client.end(function(err) {
+					if (err) throw err;
+				});
+
+				if (err) {
+					console.log("Error in query: ")
+					console.log(err);
+					callback(err, null);
+				}
+
+				console.log("Results: " + JSON.stringify(result.rows));
+				callback(null, result.rows);
+			});
+
+		});
 
 	   //  var client = new pg.Client(connectionString);
 
@@ -33,32 +59,7 @@ function getAllMentors(hobby,callback) {
 
 				// })
 		
-
-		console.log("finding mentors for %s", hobby);
-
-		var sql = "SELECT u.first, u.last, l.town, hm.mentor_id, h.name, hm.greeting FROM _user u \
-			JOIN hobby_mentor hm ON u.id = hm.mentor_id \
-			JOIN hobby h ON h.id = hm.hobby_id \
-			JOIN location l ON u.location_id = l.id \
-			WHERE h.name = $1 ;"
-
-		var params = [hobby];
-
-		var query = client.query(sql, params, function(err, result) {
-
-			client.end(function(err) {
-				if (err) throw err;
-			});
-
-			if (err) {
-				console.log("Error in query: ")
-				console.log(err);
-				callback(err, null);
-			}
-
-			console.log("Results: " + JSON.stringify(result.rows));
-			callback(null, result.rows);
-		});
+		
 }
 
 module.exports = {
