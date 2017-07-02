1f525d723528238742f4f37865737f5c623e4006
prestaylor51 <prestaylor51@gmail.com>
2017-07-01 23:14:27 -0600

    can't get returning json to update <ul>

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+
+
 function getMentors() {
 	var hobbyStr = $("#hobby").val();
 
@@ -5,22 +7,37 @@ function getMentors() {
 
 	console.log("for hobby: %s", hobbyStr);
 
-	// var params = {hobby: hobbyStr};
+	var params = {hobby: hobbyStr};
 
-	// $get("/getMentors", params, funciton(data, status){
+	$.get("/getMentors", params, function(data, status){
 
-	// 	console.log("Back from server");
+		console.log("Back from server");
+		console.log("status; %s",status);
 
-	// 	console.log(data);
+		console.log(data);
 
-	// 	updateDiv(data);
-	// });
+		updateListMentors(data);
+	});
 
 }
 
-function updateDiv(data) {
+function updateListMentors(data) {
+	
+	if (data.Search && data.Search.length > 0) {
+		var mentorList = $("#mentorListResults");
+		mentorList.empty();
 
+		console.log("updateListMentors");
+		console.log(data.Search[0].first);
 
+		for (var i = 0; i < data.Search.length; i++) {
+
+			var mentorName = data.Search[i].first;
+
+			mentorList.append("<li><p>" + mentorName + "</p></li>");
+
+		}
+	}
 
 }
 
