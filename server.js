379b1067c007303beb83842998eaf520978d76b3
prestaylor51 <prestaylor51@gmail.com>
2017-07-05 08:45:52 -0600

    added some html t omain

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/main');
+  response.redirect('main.html');
 });
 
 // GETS
